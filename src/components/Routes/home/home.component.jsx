ee72c594f8199040bd6ebc6921df08f1f88a2bc9
tayyab.mehmood <tayyab.mehmood@croeminc.com>
2026-02-17 12:13:31 +0500

    local images added

--- src/components/Routes/home/home.component.jsx
+++ src/components/Routes/home/home.component.jsx
@@ -1,32 +1,40 @@
 import Directory from "../../directory/directory.component";
+
+import hats from "../../../assets/hats.png";
+import jackets from "../../../assets/jackets.png";
+import sneakers from "../../../assets/sneakers.png";
+import womens from "../../../assets/womens.png";
+import mens from "../../../assets/men.png";
+
 const Home = () => {
     const categories = [
         {
-            "id": 1,
-            "title": "hats",
-            "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+            id: 1,
+            title: "hats",
+            imageUrl: hats,
         },
         {
-            "id": 2,
-            "title": "jackets",
-            "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+            id: 2,
+            title: "jackets",
+            imageUrl: jackets,
         },
         {
-            "id": 3,
-            "title": "sneakers",
-            "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+            id: 3,
+            title: "sneakers",
+            imageUrl: sneakers,
         },
         {
-            "id": 4,
-            "title": "womens",
-            "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+            id: 4,
+            title: "womens",
+            imageUrl: womens,
         },
         {
-            "id": 5,
-            "title": "mens",
-            "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        }
-    ]
+            id: 5,
+            title: "mens",
+            imageUrl: mens,
+        },
+    ];
+
 
 
 
